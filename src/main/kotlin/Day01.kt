2dd02735bfbe4bc5c23585e2fbec1159c85f7583
Wayne Ellis <wayne@wayneellis.co.uk>
2022-11-28 12:25:16 +0000

    Initial commit

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,18 @@
+import java.io.File
+
+fun main() {
+    Day01().puzzle1()
+    Day01().puzzle2()
+}
+
+class Day01 {
+    private val file = File("inputs/day01.txt")
+
+    fun puzzle1() {
+
+    }
+
+    fun puzzle2() {
+
+    }
+}
---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+// Replace XX with the day (2 digit format), e.g. 02
+// Create dayXX.txt file in inputs folder ready to copy data from AOC
+
+fun main() {
+    DayXX().puzzle1()
+    DayXX().puzzle2()
+}
+
+class DayXX {
+    private val file = File("inputs/dayXX.txt")
+
+    fun puzzle1() {
+
+    }
+
+    fun puzzle2() {
+
+    }
+}
