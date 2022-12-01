e000b6d9fa749e503e1815a687482d035973ccea
Wayne Ellis <wayne@wayneellis.co.uk>
2022-12-01 08:08:33 +0000

    Day 1 Complete

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -8,11 +8,33 @@ fun main() {
 class Day01 {
     private val file = File("inputs/day01.txt")
 
-    fun puzzle1() {
+    private fun getCalorieList(): MutableList<Int> {
+        val list = mutableListOf(0)
+        file.readLines().forEach {
+            if (it.isEmpty()) list.add(0)                          // add new item to tail if we find a blank line
+            else list[list.lastIndex] = list.last() + it.toInt()   // otherwise incremement the tail with the read value
+        }
+        return list
+    }
 
+
+    fun puzzle1() {
+        val list = getCalorieList()
+        // sorting is less efficient, as we only need to find the max value
+        val max = list.foldIndexed(Pair(0,0)) { idx, res, x ->
+            if (res.second < x) Pair(idx, x)
+            else res
+        }
+
+        println("Elf ${max.first} has ${max.second} calories")
     }
 
     fun puzzle2() {
+        val list = getCalorieList()
 
+        // only need to know the top3, we don't need to know the index, so just sort and take top 3
+        list.sortDescending()
+        val top3 = list.take(3).reduce { sum, x -> sum + x} // slightly more elegant/functional than //val top3 = list[0]+list[1]+list[2]
+        println("Top3 Elves are carrying $top3 Calories")
     }
 }
