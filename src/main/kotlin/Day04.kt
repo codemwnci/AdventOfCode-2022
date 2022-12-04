f539c77cb8d63066a82c8fc595cf62336390a456
Wayne Ellis <wayne@wayneellis.co.uk>
2022-12-04 07:13:03 +0000

    Day 4 Complete

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -9,7 +9,7 @@ class Day04 {
     private val file = File("inputs/day04.txt")
 
     private fun IntRange.containsAll(target: IntRange) = this.first <= target.first && this.last >= target.last
-    private fun getRangePair() = file.readLines().map { lines ->
+    private fun getRangePairs() = file.readLines().map { lines ->
         lines.split(",").map { assignments->
             assignments.split("-").let {
                 IntRange(it[0].toInt(), it[1].toInt())
@@ -18,10 +18,10 @@ class Day04 {
     }
 
     fun puzzle1() {
-        println(getRangePair().count { it[0].containsAll(it[1]) || it[1].containsAll(it[0]) })
+        println(getRangePairs().count { it[0].containsAll(it[1]) || it[1].containsAll(it[0]) })
     }
 
     fun puzzle2() {
-        println(getRangePair().count { it[0].intersect(it[1]).isNotEmpty() })
+        println(getRangePairs().count { it[0].intersect(it[1]).isNotEmpty() })
     }
 }
